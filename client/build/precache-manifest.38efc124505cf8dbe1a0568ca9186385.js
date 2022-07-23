519dd87d7c855957de427b196e4554fe73df4bb5
shankssc <suyashhbk@rediffmail.com>
2022-07-23 12:41:55 -0400

    5th commit

---
+++ client/build/precache-manifest.38efc124505cf8dbe1a0568ca9186385.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7b27b3c76cc688cb3edf962f12c294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c85f673aa8d2c94be25",
+    "url": "/static/css/main.19d8ffad.chunk.css"
+  },
+  {
+    "revision": "f1552ece64fe1f1959a6",
+    "url": "/static/js/2.b31da36a.chunk.js"
+  },
+  {
+    "revision": "a0b5b90c325aafb638d8eaf11fa9c74c",
+    "url": "/static/js/2.b31da36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c85f673aa8d2c94be25",
+    "url": "/static/js/main.623d09c3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.623d09c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "8c396de3ae5239fcfe7172de2b90392b",
+    "url": "/static/media/joseon.8c396de3.png"
+  },
+  {
+    "revision": "55af91ec82d8a2af6038955c194619a5",
+    "url": "/static/media/textl.55af91ec.png"
+  }
+]);
